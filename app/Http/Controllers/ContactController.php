9380bc27330123031e997ee25c71ec7631e14767
CODEbyPoHsiang <0939cool@gmail.com>
2019-12-05 18:43:13 +0800

    up img ok

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,10 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use App\Contact;
-
 class ContactController extends Controller
 {
     public function index()
@@ -17,14 +16,26 @@ class ContactController extends Controller
    return Contact::find($id);
 }
 
+
+
+
+
+
 public function store(Request $request)
 {
+    // request()->validate([
+    //     'ImgPath' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    // ]);
+    // $extension = request()->ImgPath->getClientOriginalExtension(); 
+    // $imageName = time() . "." . $extension;    
+    // request()->ImgPath->move(public_path('images'), $imageName);
+    
     Contact::create($request->all());
 
     return response()->json(['新增資料成功'], 200);
 }
 
-public function update(Request $request, $id)
+public function update(Request $request, $ID)
 {
     Contact::find($id)->update($request->all());
 
@@ -33,43 +44,27 @@ public function update(Request $request, $id)
 
 public function imageUploadPost(Request $request)
 
-    {
+    {   
         request()->validate([
             'ImgPath' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
-    
-        $extension = request()->ImgPath->getClientOriginalExtension(); //副檔名
-        $imageName = time() . "." . $extension;    //重新命名
+        $extension = request()->ImgPath->getClientOriginalExtension(); 
+        $imageName = time() . "." . $extension;    
+        $img1path = request()->ImgPath->move(('images'), $imageName);
+
+        Contact::insert(
+            [
+                 'ImgPath' =>  $img1path,
+            ]
+        );
+
+        // Contact::create($request->all());
+        return response()->json(['你已成功上傳頭像'], 200);
+
         
-        
-        $path=request()->ImgPath->move(public_path().'/uploads', $imageName); //移動至指定目錄
-
-        // if(isset($_FILES['ImgPath'])){ //如果表單夾帶檔案送出的話
-        //     $filename = $_FILES['ImgPath']['name']; //抓出檔案的名字
-        //     $tmp_name = $_FILES['ImgPath']['tmp_name'];//抓出檔案的站存名稱
-        //     move_uploaded_file($tmp_name, "upload/$filename");//移動檔案到upload資料夾	
-        // }
-
-
-        // $imageName = time().'.'.request()->ImgPath->getClientOriginalExtension();       
-        // request()->ImgPath->move(public_path('upload'), $imageName);
-        
-        Contact::create($request->all());
-
-      return response()->json(['你已成功上傳頭像'], 200);
-
-           
-
     }
     
-    // public function getImageUrlAttribute()
-    // {
-    //     // 如果 image 字段本身就已经是完整的 url 就直接返回
-    //     if (Str::startsWith($this->attributes['ImgPath'])) {
-    //         return $this->attributes['ImgPath'];
-    //     }
-    //     return \Storage::disk('public\upload')->url($this->attributes['ImgPath']);
-    // }
+    
 
 public function destroy($id)
 {
--- database/migrations/2019_12_03_011426_create_contacts_table.php
+++ database/migrations/2019_12_03_011426_create_contacts_table.php
@@ -14,7 +14,7 @@ class CreateContactsTable extends Migration
     public function up()
     {
         Schema::create('contacts', function (Blueprint $table) {
-            $table->bigIncrements('ID');
+            $table->bigIncrements('id');
             $table->string('Fname')->nullable();
             $table->string('Lname')->nullable();
             $table->string('Email')->nullable();
